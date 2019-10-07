d4dc9bc4ae5fdda2cc6520adb1008f1c5c5653b1
mfaust35 <5989492+mfaust35@users.noreply.github.com>
2019-10-07 11:10:27 -0700

    Setup backend for displaying cards for review

--- app/src/main/java/com/faust/m/flashcardm/presentation/review/ReviewActivity.kt
+++ app/src/main/java/com/faust/m/flashcardm/presentation/review/ReviewActivity.kt
@@ -2,7 +2,6 @@ package com.faust.m.flashcardm.presentation.review
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.faust.m.core.domain.Card
 import com.faust.m.flashcardm.R
 import com.faust.m.flashcardm.presentation.LiveDataObserver
 import com.faust.m.flashcardm.presentation.add_card.BOOKLET_ID
@@ -21,10 +20,11 @@ class ReviewActivity: AppCompatActivity(), LiveDataObserver {
         val bookletId = intent.getLongExtra(BOOKLET_ID, 0)
         viewModel = provideBookletViewModel(bookletId)
         // Setup observe data in viewModel
-        viewModel.cards().observe(this, ::onCardsChanged)
+        viewModel.getCurrentCard().observe(this, ::onCurrentCardChanged)
     }
 
-    private fun onCardsChanged(cards: List<Card>) {
-        toast("There are ${cards.size} card on this booklet")
+
+    private fun onCurrentCardChanged(currentCard: CurrentCard) {
+        toast("The current card's front value is ${currentCard.front}")
     }
 }
--- app/src/main/java/com/faust/m/flashcardm/presentation/review/ReviewViewModel.kt
+++ app/src/main/java/com/faust/m/flashcardm/presentation/review/ReviewViewModel.kt
@@ -1,35 +1,55 @@
 package com.faust.m.flashcardm.presentation.review
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.faust.m.core.data.CardRepository
 import com.faust.m.core.domain.Card
-import com.faust.m.flashcardm.presentation.MutableLiveList
+import com.faust.m.flashcardm.presentation.review.CurrentCard.State.ASKING
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import org.koin.core.KoinComponent
 import org.koin.core.inject
+import java.util.*
 
 class ReviewViewModel(private val bookletId: Long): ViewModel(), KoinComponent {
 
     private val cardRepository: CardRepository by inject()
 
-    private val cards: MutableLiveList<Card> by lazy {
-        MutableLiveList<Card>().apply {
+    private val currentCard: MutableLiveData<CurrentCard> = MutableLiveData()
+
+    private val cardQueue: Queue<Card> =
+        LinkedList<Card>().apply {
             GlobalScope.launch {
-                loadBookletCards()
+                loadQueue()
             }
         }
+
+    fun getCurrentCard(): LiveData<CurrentCard> = currentCard
+
+    private fun loadQueue() {
+        cardRepository
+            .getAllCardsForBooklet(bookletId)
+            .forEach { cardQueue.add(it) }
+        if (cardQueue.isNotEmpty()) {
+            cardQueue.remove()
+                .let {
+                    CurrentCard(
+                        it.frontAsTextOrNull() ?: "??",
+                        it.backAsTextOrNull() ?: "??",
+                        ASKING)
+                }
+                .run {
+                    currentCard.postValue(this)
+                }
+        }
+        else {
+            currentCard.postValue(CurrentCard("???", "???", ASKING))
+        }
     }
+}
 
-    fun cards(): LiveData<out List<Card>> = cards
+data class CurrentCard(val front: String, val back: String, val state: State) {
 
-    private fun loadBookletCards() =
-        mutableListOf<Card>()
-            .apply {
-                this.addAll(cardRepository.getAllCardsForBooklet(bookletId))
-            }
-            .also {
-                cards.postValue(it)
-            }
+    enum class State { ASKING, RATING }
 }
